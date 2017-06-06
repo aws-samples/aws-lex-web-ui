6d0d411f9cbec17efde3b0b954b7d33cafc1d25c
potterve <potterve@amazon.com>
2017-06-06 08:28:02 -0600

    Copyright, Amazon Software License, version 0.5.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
+## [0.5.1] - 2017-06-06
+### Changed
+- Copyrights and Amazon software license
+  
 ## [0.5.0] - 2017-06-05
 ### Added
 - Ability to deploy a sample bot based on the OrderFlowers sample
--- templates/custom-resources/cfnresponse.py
+++ templates/custom-resources/cfnresponse.py
@@ -1,9 +1,13 @@
-#  Copyright 2016-2017 Amazon Web Services, Inc. or its affiliates. All Rights Reserved.
-#  This file is licensed to you under the AWS Customer Agreement (the "License").
-#  You may not use this file except in compliance with the License.
-#  A copy of the License is located at http://aws.amazon.com/agreement/ .
-#  This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or implied.
-#  See the License for the specific language governing permissions and limitations under the License.
+# Copyright 2016-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Amazon Software License (the "License"). You may not use this file
+# except in compliance with the License. A copy of the License is located at
+#
+# http://aws.amazon.com/asl/
+#
+# or in the "license" file accompanying this file. This file is distributed on an "AS IS"
+# BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or implied. See the
+# License for the specific language governing permissions and limitations under the License.
 
 from botocore.vendored import requests
 import json
