b8b0043e332397c0aa708134f02dc9c81a6c6c60
Oliver Atoa <oliver.atoa@gmail.com>
2018-01-05 08:04:31 -0500

    fix dev middleware error display after version update

--- lex-web-ui/build/dev-server.js
+++ lex-web-ui/build/dev-server.js
@@ -27,6 +27,7 @@ var compiler = webpack(webpackConfig)
 
 var devMiddleware = require('webpack-dev-middleware')(compiler, {
   publicPath: webpackConfig.output.publicPath,
+  stats: 'errors-only',
   quiet: true
 })
 
