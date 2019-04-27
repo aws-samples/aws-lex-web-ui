db7ef430815778d5eea6e34554085d721fea3c34
bobpskier <potterve@amazon.com>
2019-04-27 16:06:47 -0600

    Removed use of nodejs 6 and switched to nodejs 8.11

--- dist/lex-web-ui.js
+++ dist/lex-web-ui.js
@@ -7415,7 +7415,7 @@ var debugs = {};
 var debugEnviron;
 exports.debuglog = function(set) {
   if (isUndefined(debugEnviron))
-    debugEnviron = Object({"NODE_ENV":"production","PACKAGE_VERSION":"0.14.5","DIST_BUILD":"true","PUBLIC_PATH":"/"}).NODE_DEBUG || '';
+    debugEnviron = Object({"NODE_ENV":"production","PACKAGE_VERSION":"0.14.6","DIST_BUILD":"true","PUBLIC_PATH":"/"}).NODE_DEBUG || '';
   set = set.toUpperCase();
   if (!debugs[set]) {
     if (new RegExp('\\b' + set + '\\b', 'i').test(debugEnviron)) {
@@ -32780,7 +32780,7 @@ License for the specific language governing permissions and limitations under th
 
 
 /* harmony default export */ __webpack_exports__["a"] = ({
-  version:  true ? "0.14.5" : '0.0.0',
+  version:  true ? "0.14.6" : '0.0.0',
   lex: {
     acceptFormat: 'audio/ogg',
     dialogState: '',
