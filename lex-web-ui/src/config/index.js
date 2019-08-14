ede3f7b43f29ebdc17e88452b06d6eb3ae9fbd21
Christensen <tchramzn@8c85904c9396.ant.amazon.com>
2019-08-14 14:05:53 -0400

    test break 10

--- lex-web-ui/src/config/index.js
+++ lex-web-ui/src/config/index.js
@@ -303,7 +303,7 @@ function getConfigFromQuery(query) {
  *
  * If deep is set to true, the merge is done recursively in both directions.
  */
-export function mergeConfig(baseConfig, srcConfig, deep = false) {
+export function mergeConfig(baseConfig, srcConfig, deep = true) {
   function mergeValue(base, src, key, shouldMergeDeep) {
     // nothing to merge as the base key is not found in the src
     if (!(key in src)) {
