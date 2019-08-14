388c61da9748df85c36b87cccf5d5e7883f8e8b6
EC2 Default User <ec2-user@ip-172-31-87-212.ec2.internal>
2019-08-14 19:25:28 +0000

    change bot name to prod

--- config/env.mk
+++ config/env.mk
@@ -17,7 +17,7 @@ export AWS_DEFAULT_PROFILE ?= default
 export AWS_DEFAULT_REGION ?= us-east-1
 
 # lex-web-ui config variables
-export BOT_NAME ?= OrderFlowers
+export BOT_NAME = QNA_prod_dev_master_one_BotYu
 # set to empty if not present in environment
 export POOL_ID ?= $()
 
