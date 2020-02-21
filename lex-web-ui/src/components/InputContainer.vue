2ebe2afff43768e2bfd7814bdca591e93e89e551
Paddy Keane <Paddy.Keane@akqa.com>
2020-02-21 17:56:34 +0000

    [~] footer tag changed to a div as it's content doesn't conform to normal content of a footer so adding role="contentinfo" isn't applicable.

--- lex-web-ui/src/components/InputContainer.vue
+++ lex-web-ui/src/components/InputContainer.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-footer app fixed>
+  <div app fixed>
     <v-layout
       row
       justify-space-between
@@ -65,7 +65,7 @@
         </v-btn>
       </v-toolbar>
     </v-layout>
-  </v-footer>
+  </div>
 </template>
 
 <script>
@@ -257,7 +257,7 @@ export default {
 };
 </script>
 <style>
-.footer {
+.input-container {
   /* make footer same height as dense toolbar */
   min-height: 48px;
 }
