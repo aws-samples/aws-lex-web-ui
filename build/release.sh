e9420f1a59c5d36f3dea695cdb2ac5ed6bc70d49
Bob Potterveld <potterve@amazon.com>
2020-12-07 07:18:37 -0700

    update release.sh to support timestamp update across unix platforms.

--- build/release.sh
+++ build/release.sh
@@ -1,6 +1,14 @@
 #! /bin/bash
 timestamp=$(date +%s)
-sed -i '' "s/Timestamp:.*/Timestamp: $timestamp/g" ../templates/master.yaml
+unamestr=$(uname)
+case $unamestr in
+"Darwin" | "FreeBSD")
+sed -i '' "s/Timestamp:.*/Timestamp: $timestamp/g" ../templates/master.yaml;;
+"Linux")
+sed -i "s/Timestamp:.*/Timestamp: $timestamp/g" ../templates/master.yaml;;
+*)
+sed -i "s/Timestamp:.*/Timestamp: $timestamp/g" ../templates/master.yaml;;
+esac
 cd ../lex-web-ui
 npm run build
 npm run build-dist
