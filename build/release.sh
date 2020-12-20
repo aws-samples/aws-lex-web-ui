ceb6b3047804b5e5cc3edcdd123ec097b1afa92a
Bob Potterveld <potterve@amazon.com>
2020-12-20 08:38:36 -0700

    enhance build script, release.sh, with simpler sed logic.

--- build/release.sh
+++ build/release.sh
@@ -5,32 +5,44 @@ export version=v$(node -p "require('../package.json').version")
 echo version is $version
 case $unamestr in
 "Darwin" | "FreeBSD")
-sed -i '' "s/(v.*)/($version)/g" ../templates/master.yaml;
-sed -i '' "s/(v.*)/($version)/g" ../templates/master-pipeline.yaml;
-sed -i '' "s/Timestamp:.*/Timestamp: $timestamp/g" ../templates/master.yaml;
-sed -i '' "s/custom-resources-.*zip/custom-resources-$version.zip/g" ../templates/master.yaml;
-sed -i '' "s/src-.*zip/src-$version.zip/g" ../templates/master.yaml;
-sed -i '' "s/Timestamp:.*/Timestamp: $timestamp/g" ../templates/master-pipeline.yaml;
-sed -i '' "s/custom-resources-.*zip/custom-resources-$version.zip/g" ../templates/master-pipeline.yaml;
-sed -i '' "s/src-.*zip/src-$version.zip/g" ../templates/master-pipeline.yaml;;
+sed -i '' -e "s/(v.*)/($version)/g" \
+-e "s/Timestamp:.*/Timestamp: $timestamp/g" \
+-e "s/custom-resources-.*zip/custom-resources-$version.zip/g" \
+-e "s/src-.*zip/src-$version.zip/g" \
+../templates/master.yaml;
+
+sed -i '' -e "s/(v.*)/($version)/g" \
+-e "s/Timestamp:.*/Timestamp: $timestamp/g" \
+-e "s/custom-resources-.*zip/custom-resources-$version.zip/g" \
+-e "s/src-.*zip/src-$version.zip/g" \
+../templates/master-pipeline.yaml;;
+
 "Linux")
-sed -i "s/(v.*)/($version)/g" ../templates/master.yaml;
-sed -i "s/(v.*)/($version)/g" ../templates/master-pipeline.yaml;
-sed -i "s/Timestamp:.*/Timestamp: $timestamp/g" ../templates/master.yaml;
-sed -i "s/custom-resources-.*zip/custom-resources-$version.zip/g" ../templates/master.yaml;
-sed -i "s/src-.*zip/src-$version.zip/g" ../templates/master.yaml;
-sed -i "s/Timestamp:.*/Timestamp: $timestamp/g" ../templates/master-pipeline.yaml;
-sed -i "s/custom-resources-.*zip/custom-resources-$version.zip/g" ../templates/master-pipeline.yaml;
-sed -i "s/src-.*zip/src-$version.zip/g" ../templates/master-pipeline.yaml;;
+sed -i -e "s/(v.*)/($version)/g" \
+-e "s/Timestamp:.*/Timestamp: $timestamp/g" \
+-e "s/src-.*zip/src-$version.zip/g" \
+-e "s/custom-resources-.*zip/custom-resources-$version.zip/g" \
+../templates/master.yaml;
+
+sed -i -e "s/(v.*)/($version)/g" \
+-e "s/Timestamp:.*/Timestamp: $timestamp/g" \
+-e "s/custom-resources-.*zip/custom-resources-$version.zip/g" \
+-e "s/src-.*zip/src-$version.zip/g" \
+../templates/master-pipeline.yaml;;
+
 *)
-sed -i "s/(v.*)/($version)/g" ../templates/master.yaml;
-sed -i "s/(v.*)/($version)/g" ../templates/master-pipeline.yaml;
-sed -i "s/Timestamp:.*/Timestamp: $timestamp/g" ../templates/master.yaml;
-sed -i "s/custom-resources-.*zip/custom-resources-$version.zip/g" ../templates/master.yaml;
-sed -i "s/src-.*zip/src-$version.zip/g" ../templates/master.yaml;
-sed -i "s/Timestamp:.*/Timestamp: $timestamp/g" ../templates/master-pipeline.yaml;
-sed -i "s/custom-resources-.*zip/custom-resources-$version.zip/g" ../templates/master-pipeline.yaml;
-sed -i "s/src-.*zip/src-$version.zip/g" ../templates/master-pipeline.yaml;;
+sed -i -e "s/(v.*)/($version)/g" \
+-e "s/Timestamp:.*/Timestamp: $timestamp/g" \
+-e "s/src-.*zip/src-$version.zip/g" \
+-e "s/custom-resources-.*zip/custom-resources-$version.zip/g" \
+../templates/master.yaml;
+
+sed -i -e "s/(v.*)/($version)/g" \
+-e "s/Timestamp:.*/Timestamp: $timestamp/g" \
+-e "s/custom-resources-.*zip/custom-resources-$version.zip/g" \
+-e "s/src-.*zip/src-$version.zip/g" \
+../templates/master-pipeline.yaml;;
+
 esac
 cd ../lex-web-ui
 npm run build
