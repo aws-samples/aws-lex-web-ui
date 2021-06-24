a6a0f6a4c7efefef9bf14373102aa9d8131d5b4b
pdkn <keanepaddy@gmail.com>
2021-06-24 16:41:39 +0100

    Error in LexruntimeV2 can't be instantiated due to config issues

--- lex-web-ui/src/components/LexWeb.vue
+++ lex-web-ui/src/components/LexWeb.vue
@@ -161,8 +161,17 @@ export default {
         // using credentials built from the identified poolId.
         //
         // The Cognito Identity Pool should be a resource in the identified region.
-        if (this.$store.state && this.$store.state.config
-          && this.$store.state.config.region && this.$store.state.config.cognito.poolId) {
+        if (this.$store.state && this.$store.state.config) {
+          const region = this.$store.state.config.region ? this.$store.state.config.region : this.$store.state.config.cognito.region;
+          if (!region) {
+            return Promise.reject(new Error('no region found in config or config.cognito'))
+          }
+          
+          const poolId = this.$store.state.config.cognito.poolId;
+          if (!poolId) {
+            return Promise.reject(new Error('no cognito.poolId found in config'))
+          }
+
           const AWSConfigConstructor = (window.AWS && window.AWS.Config) ?
             window.AWS.Config :
             AWSConfig;
@@ -175,25 +184,27 @@ export default {
           const LexRuntimeConstructor = (window.AWS && window.AWS.LexRuntime) ?
             window.AWS.LexRuntime :
             LexRuntime;
-
+            
           const LexRuntimeConstructorV2 = (window.AWS && window.AWS.LexRuntimeV2) ?
             window.AWS.LexRuntimeV2 :
             LexRuntimeV2;
-
+            
           const credentials = new CognitoConstructor(
-            { IdentityPoolId: this.$store.state.config.cognito.poolId },
-            { region: this.$store.state.config.region },
+            { IdentityPoolId: poolId },
+            { region: region },
           );
 
           const awsConfig = new AWSConfigConstructor({
-            region: this.$store.state.config.region,
+            region: region,
             credentials,
           });
 
           this.$lexWebUi.lexRuntimeClient = new LexRuntimeConstructor(awsConfig);
           this.$lexWebUi.lexRuntimeV2Client = new LexRuntimeConstructorV2(awsConfig);
           /* eslint-disable no-console */
-          console.log(`${JSON.stringify(this.$lexWebUi.lexRuntimeV2Client)}`);
+          console.log(`lexRuntimeV2Client : ${JSON.stringify(this.$lexWebUi.lexRuntimeV2Client)}`);
+        } else {
+          return Promise.reject(new Error('no config found'))
         }
 
         Promise.all([
