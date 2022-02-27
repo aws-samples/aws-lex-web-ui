64433399503c3b6fe42933b62c01f8e5d4909baa
Christopher Lott <cmlott@amazon.com>
2022-02-27 13:23:53 -0800

    fix bug where if you typed the live chat string inside a live chat, it would start a new live chat

--- lex-web-ui/src/store/actions.js
+++ lex-web-ui/src/store/actions.js
@@ -479,7 +479,9 @@ export default {
       })
       .then(() => {
         const liveChatTerms = context.state.config.connect.liveChatTerms ? context.state.config.connect.liveChatTerms.split(',').map(str => str.trim()) : [];
-        if (context.state.config.ui.enableLiveChat && liveChatTerms.find(el => el === message.text.toLowerCase())) {
+        if (context.state.config.ui.enableLiveChat && 
+          liveChatTerms.find(el => el === message.text.toLowerCase()) && 
+          context.state.chatMode !== chatMode.LIVECHAT) {
           return context.dispatch('requestLiveChat');
         } else if (context.state.liveChat.status === liveChatStatus.REQUEST_USERNAME) {
           context.commit('setLiveChatUserName', message.text);
