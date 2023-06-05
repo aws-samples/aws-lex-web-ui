8dfa881aebe18c6fab03f75b96023bef20fadbc1
Austin Johnson <austintj@amazon.com>
2023-06-05 08:36:34 -0400

    add new js

---
+++ build/create-custom-css.js
@@ -0,0 +1,32 @@
+function adjustCSSRules(selector, props, sheets)
+{
+    // get stylesheet(s)
+    if (!sheets) sheets = [...document.styleSheets];
+    else if (sheets.sup){    // sheets is a string
+        let absoluteURL = new URL(sheets, document.baseURI).href;
+        sheets = [...document.styleSheets].filter(i => i.href == absoluteURL);
+        }
+    else sheets = [sheets];  // sheets is a stylesheet
+
+    // CSS (& HTML) reduce spaces in selector to one.
+    selector = selector.replace(/\s+/g, ' ');
+    const findRule = s => [...s.cssRules].reverse().find(i => i.selectorText == selector)
+    let rule = sheets.map(findRule).filter(i=>i).pop()
+
+    const propsArr = props.sup
+        ? props.split(/\s*;\s*/).map(i => i.split(/\s*:\s*/)) // from string
+        : Object.entries(props);                              // from Object
+
+    if (rule) for (let [prop, val] of propsArr){
+        // rule.style[prop] = val; is against the spec, and does not support !important.
+        rule.style.setProperty(prop, ...val.split(/ *!(?=important)/));
+        }
+    else {
+        sheet = sheets.pop();
+        if (!props.sup) props = propsArr.reduce((str, [k, v]) => `${str}; ${k}: ${v}`, '');
+        sheet.insertRule(`${selector} { ${props} }`, sheet.cssRules.length);
+        }
+}
+
+var css_location = '../dist/custom-chatbot-style.css'
+if (process.env['messageTextColor']) { adjustCSSRules('.message-text', 'color: red', css_location )}
