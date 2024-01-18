430a2568108e07d48e07bd0f2ba352b2ad591b23
Austin Johnson <austintj@amazon.com>
2024-01-18 13:44:44 -0500

    Upgrade to Node 18 and modify Lambdas for SDK v3

--- src/streaming-lambda/index.js
+++ src/streaming-lambda/index.js
@@ -1,24 +1,26 @@
-const AWS = require('aws-sdk');
-const ddb = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10', region: process.env.AWS_REGION });
+const { PutCommand, DynamoDBDocumentClient } = require('@aws-sdk/lib-dynamodb');
+const { DynamoDBClient } = require('@aws-sdk/client-dynamodb');
+
+const client = new DynamoDBClient({ apiVersion: '2012-08-10', region: process.env.AWS_REGION });
+const docClient = DynamoDBDocumentClient.from(client);
 
 exports.handler = async event => {
     console.log("Received event: " + JSON.stringify(event));
     const ttlTime = Date.now() / 1000 + 86400; //One day later in epoch time for TTL
 
-    const putParams = {
+    const command = new PutCommand({
         TableName: process.env.TABLE_NAME,
         Item: {
             connectionId: event.requestContext.connectionId,
             sessionId: event.queryStringParameters.sessionId,
             ttl: ttlTime
         }
-    };
-
+    });
+    
     try {
-        await ddb.put(putParams).promise();
+        await docClient.send(command);
+        return { statusCode: 200, body: 'Connected.' };
     } catch (err) {
         return { statusCode: 500, body: 'Failed to connect: ' + JSON.stringify(err) };
     }
-
-    return { statusCode: 200, body: 'Connected.' };
 };
